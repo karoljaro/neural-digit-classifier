f412309a74d94262b268aeb053949fba1346475f
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-09-05 22:29:39 +0200

    feat: Load and process image data in main function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 from image import Image
 from pathlib import Path
+import numpy as np
 
 
 def main() -> None:
     image = Image()
     current_dir = Path.cwd()
     file_path = current_dir / "sample" / "one.png"
-    print(image.load(file_path))
+    loaded_image = image.load(file_path)
+
+    x = loaded_image.reshape(-1, 1)
+
+    rng = np.random.default_rng(42)
+
+    W = rng.normal(
+        loc=0.0,
+        scale=0.01,
+        size=(64, 784)
+    ).astype(np.float32)
+
+    b = np.zeros((64, 1), dtype=np.float32)
+
+    z = W @ x + b
+    print(z)
 
 
 if __name__ == "__main__":
